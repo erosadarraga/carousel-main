98bb21bf5600a7fe45a3da0da93272b6e0180046
Eros Jose Adarraga Jimenez <eroslbac123456@gmail.com>
2022-05-31 15:47:19 -0500

    m

--- src/components/sliderImage.js
+++ src/components/sliderImage.js
@@ -2,6 +2,7 @@ import First from "../assets/foto2.jpg";
 import Second from "../assets/foto3.jpg";
 import Third from "../assets/foto5.jpg";
 import four from "../assets/foto4.jpg";
+import Five from "../assets/FOTO1 REAL.jpg";
 
 const matriz = [
   {
@@ -23,6 +24,11 @@ const matriz = [
     title: "Third Slide",
     description: "https://chat.whatsapp.com/EeqPiOVPwf2Hqe4od7YLA3",
     urls: four,
+  },
+  {
+    title: "Third Slide",
+    description: "https://chat.whatsapp.com/EeqPiOVPwf2Hqe4od7YLA3",
+    urls: Five,
   },
 ];
 
